abbfe41089b78c6653017dc0d97c5206fb84c9c5
Mariano Reingart <reingart@gmail.com>
2013-01-24 21:27:16 -0300

    added object tag handler (missing file)

---
+++ gui2py/html/object.py
@@ -0,0 +1,119 @@
+# based on wx.lib.wxpTag (Robin Dunn 1999, Jeff Grimmett 2003 wxWindows license)
+
+from . import GetParam, input
+from .input import FormControlMixin
+
+from .. import registry 
+
+
+import  wx
+import  wx.html
+
+
+class ObjectTagHandler(wx.html.HtmlWinTagHandler):
+    def __init__(self):
+        wx.html.HtmlWinTagHandler.__init__(self)
+        self.params = {}
+
+    def GetSupportedTags(self):
+        return "OBJECT,PARAM"
+
+    def HandleTag(self, tag):
+        name = tag.GetName()
+        if name == "OBJECT":
+            return self.HandleObjectTag(tag)
+        elif name == "PARAM":
+            return self.HandleParamTag(tag)
+        else:
+            raise ValueError('unknown tag: %s' % name)
+
+
+    def HandleObjectTag(self, tag):
+        # create a new dict to store params
+        self.params = {}
+        # find and verify the class
+        if not tag.HasParam('CLASS'):
+            raise AttributeError("OBJECT tag requires a CLASS attribute")
+        class_name = tag.GetParam('CLASS')
+        if class_name not in registry.CONTROLS:
+            raise TypeError("OBJECT tag attribute CLASS must name a class")
+        obj_class = registry.CONTROLS[class_name]
+
+        parent = self.GetParser().GetWindowInterface().GetHTMLWindow()
+
+        # now look for width and height
+        width = -1
+        height = -1
+        float_width = 0
+        if tag.HasParam('WIDTH'):
+            width = tag.GetParam('WIDTH')
+            if width[-1] == '%':
+                float_width = int(width[:-1], 0)
+                width = float_width
+            else:
+                width = int(width)
+        if tag.HasParam('HEIGHT'):
+            height = tag.GetParam('HEIGHT')
+            if height[-1] == '%':
+                ##import pdb; pdb.set_trace()
+                # float_height is not implemented by wx, use parent height
+                float_height = int(height[:-1], 0)
+                height = parent.Parent.GetSize()[1]*float_height/100 
+            else:
+                height = int(height)
+        self.params['size'] = (width, height)
+
+        # parse up to the closing tag, and gather any nested Param tags.
+        self.ParseInner(tag)
+
+        # create the object
+        parent = self.GetParser().GetWindowInterface().GetHTMLWindow()
+        if parent:
+            
+            try:
+                obj = obj_class(parent, **self.params)
+            except Exception, e:
+                import pdb; pdb.set_trace()
+            obj.visible = True
+            # add it to the HtmlWindow
+            self.GetParser().GetContainer().InsertCell(
+                wx.html.HtmlWidgetCell(obj.wx_obj, float_width))
+        return True
+
+
+    def HandleParamTag(self, tag):
+        ##import pdb; pdb.set_trace()
+        if not tag.HasParam('NAME'):
+            return False
+
+        name = tag.GetParam('NAME')
+        value = ""
+        if tag.HasParam('VALUE'):
+            value = tag.GetParam('VALUE')
+
+        if not value:
+            value = None
+        elif value[0] == '#':
+            # convert to wx.Colour
+            try:
+                red   = int('0x'+value[1:3], 16)
+                green = int('0x'+value[3:5], 16)
+                blue  = int('0x'+value[5:], 16)
+                value = wx.Colour(red, green, blue)
+            except:
+                pass
+        else:
+            # check for something that should be evaluated
+            try:
+                value = eval(value)
+            except Exception, e:
+                import pdb; pdb.set_trace()
+
+        self.params[name] = value
+        return False
+
+
+wx.html.HtmlWinParser_AddTagHandler(ObjectTagHandler)
+
+        
+        
