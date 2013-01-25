3cc9c48e8f00adbac5428f96afc2f66d0906c14d
Mariano Reingart <reingart@gmail.com>
2013-01-24 21:28:01 -0300

    added HtmlWindow (missing file)

---
+++ gui2py/windows/htmlwin.py
@@ -0,0 +1,41 @@
+import wx
+
+from ..components import Component, Spec, InitSpec, EventSpec, StyleSpec
+from ..controls import HtmlBox
+from ..windows import Window
+from ..event import UIEvent
+
+
+class HtmlWindow(Window):
+    "A window that contains a html document (including embedded controls)" 
+
+    def __init__(self, parent=None, **kwargs):
+        Window.__init__(self, parent=parent, **kwargs)
+        self.html = HtmlBox(self, name="document", scrollbars=True)
+    
+    def open(self, location=None):
+        if not location:
+            print "none!"
+            pass
+        elif location.startswith(("http://", "https://", "file://")):
+            print "loading page", location
+            self.html.load_page(location)
+        else:
+            print "loading file", location
+            self.html.load_file(location)
+        print "done!"
+
+    def write(self, text):
+        self.html.write(text)
+ 
+
+if __name__ == "__main__":
+    # basic test until proper unit_test
+    from ..controls import Button
+    app = wx.App(redirect=False)
+    w = HtmlWindow(title="hello world", name="frmTest", resizable=True, 
+                   visible=False)
+    #w.open("http://www.google.com/")
+    w.write("<a href='hola'>hello</a")
+    w.show()
+    app.MainLoop()
